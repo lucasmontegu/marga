d63491d991271cae3c34b20b719c19875ccda761
Lucas Montegú <38597358+lucasmontegu@users.noreply.github.com>
2023-09-17 16:57:25 +0100

    Fix tailwind errors

--- components/Pricing.tsx
+++ components/Pricing.tsx
@@ -1,11 +1,13 @@
+/* eslint-disable tailwindcss/migration-from-tailwind-2 */
 'use client';
 
-import Button from '@/components/ui/Button';
+import {Button} from '@/components/ui/button';
 import { Database } from '@/types_db';
 import { postData } from '@/utils/helpers';
 import { getStripe } from '@/utils/stripe-client';
 import { Session, User } from '@supabase/supabase-js';
 import cn from 'classnames';
+import Image from 'next/image';
 import { useRouter } from 'next/navigation';
 import { useState } from 'react';
 
@@ -75,8 +77,8 @@ export default function Pricing({
   if (!products.length)
     return (
       <section className="bg-black">
-        <div className="max-w-6xl px-4 py-8 mx-auto sm:py-24 sm:px-6 lg:px-8">
-          <div className="sm:flex sm:flex-col sm:align-center"></div>
+        <div className="mx-auto max-w-6xl px-4 py-8 sm:px-6 sm:py-24 lg:px-8">
+          <div className="sm:align-center sm:flex sm:flex-col"></div>
           <p className="text-4xl font-extrabold text-white sm:text-center sm:text-6xl">
             No subscription pricing plans found. Create them in your{' '}
             <a
@@ -97,23 +99,24 @@ export default function Pricing({
   if (products.length === 1)
     return (
       <section className="bg-black">
-        <div className="max-w-6xl px-4 py-8 mx-auto sm:py-24 sm:px-6 lg:px-8">
-          <div className="sm:flex sm:flex-col sm:align-center">
+        <div className="mx-auto max-w-6xl px-4 py-8 sm:px-6 sm:py-24 lg:px-8">
+          <div className="sm:align-center sm:flex sm:flex-col">
             <h1 className="text-4xl font-extrabold text-white sm:text-center sm:text-6xl">
               Pricing Plans
             </h1>
-            <p className="max-w-2xl m-auto mt-5 text-xl text-zinc-200 sm:text-center sm:text-2xl">
+            <p className="m-auto mt-5 max-w-2xl text-xl text-zinc-200 sm:text-center sm:text-2xl">
               Start building for free, then add a site plan to go live. Account
               plans unlock additional features.
             </p>
-            <div className="relative flex self-center mt-12 border rounded-lg bg-zinc-900 border-zinc-800">
-              <div className="border border-pink-500 border-opacity-50 divide-y rounded-lg shadow-sm bg-zinc-900 divide-zinc-600">
-                <div className="p-6 py-2 m-1 text-2xl font-medium text-white rounded-md shadow-sm border-zinc-800 whitespace-nowrap focus:outline-none focus:ring-2 focus:ring-pink-500 focus:ring-opacity-50 focus:z-10 sm:w-auto sm:px-8">
+            <div className="relative mt-12 flex self-center rounded-lg border border-zinc-800 bg-zinc-900">
+              <div className="divide-y divide-zinc-600 rounded-lg border border-pink-500 border-opacity-50 bg-zinc-900 shadow-sm">
+                
+                <div className="m-1 whitespace-nowrap rounded-md border-zinc-800 p-6 py-2 text-2xl font-medium text-white shadow-sm focus:z-10 focus:outline-none focus:ring-2 focus:ring-pink-500 focus:ring-opacity-50 sm:w-auto sm:px-8">
                   {products[0].name}
                 </div>
               </div>
             </div>
-            <div className="mt-6 space-y-4 sm:mt-12 sm:space-y-0 sm:grid sm:grid-cols-2 sm:gap-6 lg:max-w-4xl lg:mx-auto xl:max-w-none xl:mx-0 xl:grid-cols-3">
+            <div className="mt-6 space-y-4 sm:mt-12 sm:grid sm:grid-cols-2 sm:gap-6 sm:space-y-0 lg:mx-auto lg:max-w-4xl xl:mx-0 xl:max-w-none xl:grid-cols-3">
               {products[0].prices?.map((price) => {
                 const priceString =
                   price.unit_amount &&
@@ -126,11 +129,11 @@ export default function Pricing({
                 return (
                   <div
                     key={price.interval}
-                    className="divide-y rounded-lg shadow-sm divide-zinc-600 bg-zinc-900"
+                    className="divide-y divide-zinc-600 rounded-lg bg-zinc-900 shadow-sm"
                   >
                     <div className="p-6">
                       <p>
-                        <span className="text-5xl font-extrabold white">
+                        <span className="white text-5xl font-extrabold">
                           {priceString}
                         </span>
                         <span className="text-base font-medium text-zinc-100">
@@ -139,12 +142,10 @@ export default function Pricing({
                       </p>
                       <p className="mt-4 text-zinc-300">{price.description}</p>
                       <Button
-                        variant="slim"
                         type="button"
                         disabled={false}
-                        loading={priceIdLoading === price.id}
                         onClick={() => handleCheckout(price)}
-                        className="block w-full py-2 mt-12 text-sm font-semibold text-center text-white rounded-md hover:bg-zinc-900 "
+                        className="mt-12 block w-full rounded-md py-2 text-center text-sm font-semibold text-white hover:bg-zinc-900 "
                       >
                         {products[0].name ===
                         subscription?.prices?.products?.name
@@ -164,25 +165,26 @@ export default function Pricing({
 
   return (
     <section className="bg-black">
-      <div className="max-w-6xl px-4 py-8 mx-auto sm:py-24 sm:px-6 lg:px-8">
-        <div className="sm:flex sm:flex-col sm:align-center">
+      <div className="mx-auto max-w-6xl px-4 py-8 sm:px-6 sm:py-24 lg:px-8">
+        <div className="sm:align-center sm:flex sm:flex-col">
           <h1 className="text-4xl font-extrabold text-white sm:text-center sm:text-6xl">
             Pricing Plans
           </h1>
-          <p className="max-w-2xl m-auto mt-5 text-xl text-zinc-200 sm:text-center sm:text-2xl">
+          <p className="m-auto mt-5 max-w-2xl text-xl text-zinc-200 sm:text-center sm:text-2xl">
             Start building for free, then add a site plan to go live. Account
             plans unlock additional features.
           </p>
-          <div className="relative self-center mt-6 bg-zinc-900 rounded-lg p-0.5 flex sm:mt-8 border border-zinc-800">
+          <div className="relative mt-6 flex self-center rounded-lg border border-zinc-800 bg-zinc-900 p-0.5 sm:mt-8">
             {intervals.includes('month') && (
               <button
                 onClick={() => setBillingInterval('month')}
                 type="button"
+                // eslint-disable-next-line tailwindcss/migration-from-tailwind-2
                 className={`${
                   billingInterval === 'month'
-                    ? 'relative w-1/2 bg-zinc-700 border-zinc-800 shadow-sm text-white'
-                    : 'ml-0.5 relative w-1/2 border border-transparent text-zinc-400'
-                } rounded-md m-1 py-2 text-sm font-medium whitespace-nowrap focus:outline-none focus:ring-2 focus:ring-pink-500 focus:ring-opacity-50 focus:z-10 sm:w-auto sm:px-8`}
+                    ? 'relative w-1/2 border-zinc-800 bg-zinc-700 text-white shadow-sm'
+                    : 'relative ml-0.5 w-1/2 border border-transparent text-zinc-400'
+                } m-1 whitespace-nowrap rounded-md py-2 text-sm font-medium focus:z-10 focus:outline-none focus:ring-2 focus:ring-pink-500 focus:ring-opacity-50 sm:w-auto sm:px-8`}
               >
                 Monthly billing
               </button>
@@ -191,18 +193,19 @@ export default function Pricing({
               <button
                 onClick={() => setBillingInterval('year')}
                 type="button"
+                // eslint-disable-next-line tailwindcss/migration-from-tailwind-2
                 className={`${
                   billingInterval === 'year'
-                    ? 'relative w-1/2 bg-zinc-700 border-zinc-800 shadow-sm text-white'
-                    : 'ml-0.5 relative w-1/2 border border-transparent text-zinc-400'
-                } rounded-md m-1 py-2 text-sm font-medium whitespace-nowrap focus:outline-none focus:ring-2 focus:ring-pink-500 focus:ring-opacity-50 focus:z-10 sm:w-auto sm:px-8`}
+                    ? 'relative w-1/2 border-zinc-800 bg-zinc-700 text-white shadow-sm'
+                    : 'relative ml-0.5 w-1/2 border border-transparent text-zinc-400'
+                } m-1 whitespace-nowrap rounded-md py-2 text-sm font-medium focus:z-10 focus:outline-none focus:ring-2 focus:ring-pink-500 focus:ring-opacity-50 sm:w-auto sm:px-8`}
               >
                 Yearly billing
               </button>
             )}
           </div>
         </div>
-        <div className="mt-12 space-y-4 sm:mt-16 sm:space-y-0 sm:grid sm:grid-cols-2 sm:gap-6 lg:max-w-4xl lg:mx-auto xl:max-w-none xl:mx-0 xl:grid-cols-3">
+        <div className="mt-12 space-y-4 sm:mt-16 sm:grid sm:grid-cols-2 sm:gap-6 sm:space-y-0 lg:mx-auto lg:max-w-4xl xl:mx-0 xl:max-w-none xl:grid-cols-3">
           {products.map((product) => {
             const price = product?.prices?.find(
               (price) => price.interval === billingInterval
@@ -217,7 +220,7 @@ export default function Pricing({
               <div
                 key={product.id}
                 className={cn(
-                  'rounded-lg shadow-sm divide-y divide-zinc-600 bg-zinc-900',
+                  'divide-y divide-zinc-600 rounded-lg bg-zinc-900 shadow-sm',
                   {
                     'border border-pink-500': subscription
                       ? product.name === subscription?.prices?.products?.name
@@ -231,7 +234,7 @@ export default function Pricing({
                   </h2>
                   <p className="mt-4 text-zinc-300">{product.description}</p>
                   <p className="mt-8">
-                    <span className="text-5xl font-extrabold white">
+                    <span className="white text-5xl font-extrabold">
                       {priceString}
                     </span>
                     <span className="text-base font-medium text-zinc-100">
@@ -239,12 +242,10 @@ export default function Pricing({
                     </span>
                   </p>
                   <Button
-                    variant="slim"
                     type="button"
                     disabled={!session}
-                    loading={priceIdLoading === price.id}
                     onClick={() => handleCheckout(price)}
-                    className="block w-full py-2 mt-8 text-sm font-semibold text-center text-white rounded-md hover:bg-zinc-900"
+                    className="mt-8 block w-full rounded-md py-2 text-center text-sm font-semibold text-white hover:bg-zinc-900"
                   >
                     {subscription ? 'Manage' : 'Subscribe'}
                   </Button>
@@ -262,13 +263,13 @@ export default function Pricing({
 function LogoCloud() {
   return (
     <div>
-      <p className="mt-24 text-xs uppercase text-zinc-400 text-center font-bold tracking-[0.3em]">
+      <p className="mt-24 text-center text-xs font-bold uppercase tracking-[0.3em] text-zinc-400">
         Brought to you by
       </p>
-      <div className="flex flex-col items-center my-12 space-y-4 sm:mt-8 sm:space-y-0 md:mx-auto md:max-w-2xl sm:grid sm:gap-6 sm:grid-cols-5">
+      <div className="my-12 flex flex-col items-center space-y-4 sm:mt-8 sm:grid sm:grid-cols-5 sm:gap-6 sm:space-y-0 md:mx-auto md:max-w-2xl">
         <div className="flex items-center justify-start">
           <a href="https://nextjs.org" aria-label="Next.js Link">
-            <img
+            <Image
               src="/nextjs.svg"
               alt="Next.js Logo"
               className="h-12 text-white"
@@ -277,7 +278,7 @@ function LogoCloud() {
         </div>
         <div className="flex items-center justify-start">
           <a href="https://vercel.com" aria-label="Vercel.com Link">
-            <img
+            <Image
               src="/vercel.svg"
               alt="Vercel.com Logo"
               className="h-6 text-white"
@@ -286,7 +287,7 @@ function LogoCloud() {
         </div>
         <div className="flex items-center justify-start">
           <a href="https://stripe.com" aria-label="stripe.com Link">
-            <img
+            <Image
               src="/stripe.svg"
               alt="stripe.com Logo"
               className="h-12 text-white"
@@ -295,7 +296,7 @@ function LogoCloud() {
         </div>
         <div className="flex items-center justify-start">
           <a href="https://supabase.io" aria-label="supabase.io Link">
-            <img
+            <Image
               src="/supabase.svg"
               alt="supabase.io Logo"
               className="h-10 text-white"
@@ -304,7 +305,7 @@ function LogoCloud() {
         </div>
         <div className="flex items-center justify-start">
           <a href="https://github.com" aria-label="github.com Link">
-            <img
+            <Image
               src="/github.svg"
               alt="github.com Logo"
               className="h-8 text-white"
